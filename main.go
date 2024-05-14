2cc247ead3c7f720edf91d31d2a4abc69bf6ce65
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-05-14 11:43:32 -0400

    Rewrite script to download files locally first

--- main.go
+++ main.go
@@ -1,97 +1,78 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
-	"io"
-	"io/fs"
 	"os"
-	"strconv"
-	"sync"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
+func isDownloaded(file string) bool {
+    cmd := exec.Command("stat", "-f", "%b", file)
+    output, err := cmd.Output()
+    if err != nil {
+        return false
+    }
+    allocatedBlocks := strings.TrimSpace(string(output))
+    return allocatedBlocks != "0" // checks for allocated bytes
+}
+
+func downloadFile(file string) error {
+    cmd := exec.Command("brctl", "download", file)
+    var out bytes.Buffer
+    cmd.Stdout = &out
+    cmd.Stderr = &out
+    err := cmd.Run()
+    if err != nil {
+        fmt.Println("Error downloading:", out.String())
+    }
+    return err
+}
+
+func walkPath(dir string, iCloudBasePath string) error {
+    // Verify if the directory is within iCloud Drive
+    if !strings.HasPrefix(dir, iCloudBasePath) {
+        message := fmt.Sprintf("Error: The specified directory is not within iCloud Drive (%s).\n", iCloudBasePath)
+        return errors.New(message)
+    }
+
+    err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if !info.IsDir() {
+            if !isDownloaded(path) {
+                fmt.Printf("Downloading %s...\n", path)
+                if err := downloadFile(path); err != nil {
+                    fmt.Printf("Failed to download %s: %v\n", path, err)
+                }
+            } else {
+                fmt.Printf("%s is already downloaded.\n", path)
+            }
+        }
+        return nil
+    })
+
+	var message error
+    if err != nil {
+        temp := fmt.Sprintf("Error walking the path %s: %v\n", dir, err)
+		message = errors.New(temp)
+    }
+	return message // either error message or nil
+}
+
 func main() {
-	fmt.Println("Hello icmv!")
-	srcPath := ""
-	dstPath := ""
-	contents := getDirContents(srcPath)
-	splitContents := splitDir(contents, 100)
-	beginConcurrentOperation(splitContents, srcPath, dstPath)
-}
-
-func beginConcurrentOperation(splitContents [][]fs.DirEntry, srcPath string, dstPath string) {
-	var wg sync.WaitGroup
-	for i, subDir := range splitContents {
-		fmt.Printf("Writing to tempDir %d\n", i)
-		wg.Add(1)
-		go beginCopy(subDir, srcPath, dstPath, i, &wg)
-	}
-	wg.Wait()
-	fmt.Println("Copy finished successfully!")
-}
-
-// THIS WILL NEED TO BE EDITED FOR TEMPORARY STORAGE CAPACITY
-// OR TARGET THE SSD DIRECTLY
-func beginCopy(subDir []fs.DirEntry, srcPath string, dstPath string, dirNum int, wg *sync.WaitGroup) error {
-	defer wg.Done()
-	num := strconv.Itoa(dirNum)
-	err := os.Mkdir(dstPath+num, 0755)
-	if os.IsExist(err) {
-		err = nil
-	}
+	paths, err := os.ReadFile("config.txt")
 	if err != nil {
-		fmt.Printf("Error creating directory: %v\n", err)
-		return err
+		fmt.Println("There was a problem getting the paths.")
+		return
 	}
-	for _, item := range subDir {
-		src, err := os.Open(srcPath + "/" + item.Name())
-		if err != nil {
-			fmt.Printf("Error opening %v: %v\n", item.Name(), err)
-			return err
-		}
-		defer src.Close()
-
-		dst, err := os.Create(dstPath + num + "/" + item.Name())
-		if err != nil {
-			fmt.Printf("Error creating destination: %v\n", err)
-			return err
-		}
-		defer dst.Close()
-
-		_, err = io.Copy(dst, src)
-		if err != nil {
-			fmt.Printf("Error copying from src to dst: %v\n", err)
-			return err
-		}
-	}
-	return nil
-}
-
-func calculateStartingIndices(length int, pieceLength int) []int {
-	var result []int
-	for i := 0; i < length; i += pieceLength {
-		result = append(result, i)
-	}
-	return result
-}
-
-func splitDir(contents []fs.DirEntry, pieceLength int) [][]fs.DirEntry {
-	var result [][]fs.DirEntry
-	length := len(contents)
-	for start := 0; start < length; start += pieceLength {
-		end := start + pieceLength
-		if end > length {
-			end = length
-		}
-		result = append(result, contents[start:end])
-	}
-	return result
-}
-
-func getDirContents(path string) []fs.DirEntry {
-	contents, err := os.ReadDir(path)
-	if err != nil {
-		fmt.Printf("Unable to read directory: %v\n", err)
-		os.Exit(1)
-	}
-	return contents
+	lines := strings.Split(string(paths), "\n")
+    iCloudBasePath := lines[0]
+	directory := lines[1]
+	walkPath(directory, iCloudBasePath)
 }
