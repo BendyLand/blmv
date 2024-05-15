fd119c8204e323686448ce585e7a0f878d71f79a
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-05-15 14:02:22 -0400

    Write function to handle partitioning

--- main.go
+++ main.go
@@ -2,28 +2,66 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
 )
 
 func main() {
-	paths, err := os.ReadFile("config.txt")
+	paths, err := getPaths()
 	if err != nil {
-		fmt.Println("There was a problem getting the paths.")
+		fmt.Println(err)
 		return
 	}
-	lines := strings.Split(string(paths), "\n")
-	directory := lines[0] 
-	destination := lines[1]
-	moveDownloadedFiles(directory, destination)
+	files, err := os.ReadDir(paths.Src)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	subDirs := partitionFiles(files, 50)
+	for _, dir := range subDirs {
+		fmt.Println(dir)
+	}
 }
 
-func moveDownloadedFiles(srcPath string, dstPath string) error {
-	contents, err := os.ReadDir(srcPath)
-	if err != nil {
-		fmt.Println("Error getting contents")
-		return err
+func partitionFiles(files []fs.DirEntry, numPartitions int) [][]fs.DirEntry {
+	partitionSize := len(files) / numPartitions
+	if len(files) % partitionSize != 0 {
+		partitionSize += 1
 	}
-	fmt.Println(contents)
-	return nil
+	result := make([][]fs.DirEntry, numPartitions)
+	for _, file := range files {
+		temp := make([]fs.DirEntry, partitionSize)
+		for i := range partitionSize {
+			temp[i] = file
+		}
+		result = append(result, temp)
+	}
+	return result
+}
+
+func calculateNumPartitions(numFiles int) int {
+	numPartitions := numFiles / 100
+	if numPartitions < 1 {
+		numPartitions = 1
+	}
+	return numPartitions
+}
+
+type Paths struct {
+	Src string
+	Dst string
+}
+
+func getPaths() (Paths, error) {
+	paths, err := os.ReadFile("config.txt")
+	if err != nil {
+		message := fmt.Errorf("There was a problem getting the paths.\n")
+		return Paths{}, message
+	}
+	lines := strings.Split(string(paths), "\n")
+	source := lines[0]
+	destination := lines[1]
+	result := Paths{source, destination}
+	return result, nil
 }
